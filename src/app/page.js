8f234343bf8efc3a991ddb04aeb886d01ae847e6
kevvj <kevinjvillalba774+1@gmail.com>
2024-10-05 23:02:11 -0500

    si

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,5 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+
 import ClothesBox from "./Clothes-Box";
-import Head from 'next/head';
 
 export default function Home() {
   return (
