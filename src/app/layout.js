08e552734996dfad9cea4195d839168e6e5e2249
kevvj <kevinjvillalba774+1@gmail.com>
2024-10-05 22:51:04 -0500

    ya

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,6 +24,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
