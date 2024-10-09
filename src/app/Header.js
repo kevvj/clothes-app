d3b8740756ceaede5f94183327879e68d8eb5b15
kevvj <kevinjvillalba774+1@gmail.com>
2024-10-09 00:27:21 -0500

    length:fix

--- src/app/Header.js
+++ src/app/Header.js
@@ -35,19 +35,19 @@ const UserPanel = ({Fav}) => {
     return (
         <div className="user-panel">
             <button className="Button">
-                <FontAwesomeIcon icon={faMagnifyingGlass} size="1x" />
+                <FontAwesomeIcon icon={faMagnifyingGlass} className="custom-icon" />
 
             </button>
             <button className="Button">
                 <div className="icon-container">
-                    <FontAwesomeIcon icon={faHeart} size="1x" />
+                    <FontAwesomeIcon icon={faHeart} className="custom-icon" />
                     <span className= {Fav.length > 0 ? "notification-badge":""}>{Fav.length > 0 ? Fav.length:""}</span>
                     
                 </div>
             </button>
             <button className="Button">
                 <div className="icon-container">
-                    <FontAwesomeIcon icon={faCartShopping} size="1x"></FontAwesomeIcon>
+                    <FontAwesomeIcon icon={faCartShopping} className="custom-icon"></FontAwesomeIcon>
                     <span className="notification-badge">2</span>
                 </div>
             </button>
