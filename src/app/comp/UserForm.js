a2e7e0d4d08a59e7c644b402ead3360b8448ecf5
kevvj <kevinjvillalba774+1@gmail.com>
2024-11-09 23:11:46 -0500

    1

---
+++ src/app/comp/UserForm.js
@@ -0,0 +1,70 @@
+import { useState } from 'react';
+
+const UserForm = () => {
+
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const [isLoggedIn, setIsLoggedIn] = useState(null);
+
+    const users = [
+        { id: 1, username: "user", password: "password" },
+        { id: 2, username: "user2", password: "password2" },
+        { id: 3, username: "user3", password: "password3" },
+      ];
+
+
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+
+        const user = users.find(u => u.username === event.target.username.value)
+
+        if( user && user.password === event.target.password.value){
+            
+            setIsLoggedIn(true)
+        }else{
+            setIsLoggedIn(false)
+        }
+    }
+
+
+    return (
+        <div className="user-form-content">
+
+
+            <form onSubmit={handleSubmit}>
+
+                <h2>Iniciar Sesion</h2>
+
+                <div>
+                    <p>Usuario</p>
+                    <input
+                        type='text'
+                        values={username}
+                        name="username"
+                        placeholder='Escriba su usuario'
+                        onChange={({ target }) => setUsername(target.value)}
+                    ></input>
+                </div>
+
+                <div>
+                    <p>Contraseña</p>
+                    <input
+                        type='password'
+                        values={password}
+                        name="password"
+                        placeholder='Escriba su contraseña'
+                        onChange={({ target }) => setPassword(target.value)}
+                    ></input>
+                </div>
+
+                <button className="Button">Ingresar</button>
+
+                {isLoggedIn ===false && <p className = "error">Usuario o contraseña incorrecta</p>}
+            </form>
+
+        </div>
+    )
+}
+
+export default UserForm
