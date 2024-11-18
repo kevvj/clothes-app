72e55f3a5a8cad23f68813cf3960db9568da3ea5
kevvj <kevinjvillalba774+1@gmail.com>
2024-11-17 23:30:08 -0500

    l

--- src/app/user/page.js
+++ src/app/user/page.js
@@ -3,6 +3,8 @@
 import Header from "../comp/Header";
 import { useRouter } from 'next/navigation'
 import PrincipalBox from "../comp/Principal-Box"
+import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 
 import { isLoggedIn, setIsLoggedIn } from '../globals/LogIn'
@@ -30,7 +32,75 @@ export default function Home() {
     const UserSection = () => {
         return (
             <div className="user-section-container">
-                <div className="user-section">parte del usuario en proceso...</div>
+                <div className="user-section">
+
+                    <img src="./img/Racist_Cat.webp"></img>
+
+                    <div className="data-settings-container">
+                        <div className="settings-item">
+
+                            <h4>Nombres: </h4>
+                            <p>Kevin José</p>
+                            <button>
+                                <FontAwesomeIcon icon={faPenToSquare}></FontAwesomeIcon>
+                            </button>
+
+                        </div>
+
+                        <div className="settings-item">
+
+                            <h4>Apellidos: </h4>
+                            <p>Tapias Villalba</p>
+                            <button>
+                                <FontAwesomeIcon icon={faPenToSquare}></FontAwesomeIcon>
+                            </button>
+
+                        </div>
+
+                        <div className="settings-item">
+
+                            <h4>Nombre de usuario: </h4>
+                            <p>Inge Kevin</p>
+                            <button>
+                                <FontAwesomeIcon icon={faPenToSquare}></FontAwesomeIcon>
+                            </button>
+
+                        </div>
+
+                        <div className="settings-item">
+
+                            <h4>Correo Electronico: </h4>
+                            <p>Kevinjvillalba774@gmail.com</p>
+                            <button>
+                                <FontAwesomeIcon icon={faPenToSquare}></FontAwesomeIcon>
+                            </button>
+
+                        </div>
+
+                        <div className="settings-item-button"><button className="Button">Cambiar contraseña</button></div>
+                        
+
+                        {/* <div className="settings-item">
+                            <h4>Cambiar Contraseña:</h4>
+                            <form className="password-change-form">
+                                <input type="password" placeholder="Nueva contraseña" />
+                                <input type="password" placeholder="Confirmar contraseña" />
+                                <button type="submit">Actualizar</button>
+                            </form>
+                        </div> */}
+
+                    </div>
+
+
+
+                    {/* <form action="/ruta/donde/quieras/subir" method="POST" enctype="multipart/form-data">
+                        <label for="file-upload">Selecciona un archivo:</label>
+                        <input type="file" id="file-upload" name="file-upload" accept=".jpg,.jpeg,.png" />
+                        <button type="submit">Subir archivo</button>
+                    </form> */}
+                </div>
+
+
             </div>
         )
     }
